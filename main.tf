db98f6502ed943146e4eb08a30def54fe729a40a
Philip Dubois <hello@philipdubois.be>
2018-02-21 15:15:28 +0100

    Add cluster-wide r53 record

--- main.tf
+++ main.tf
@@ -101,17 +101,3 @@ EOF
     content      = "${module.puppet_userdata.user_datas[count.index]}"
   }
 }
-
-data "aws_route53_zone" "domain" {
-  count = "${var.r53_domain == "" ? 0 : 1}"
-  name  = "${var.environment}.${var.r53_domain}"
-}
-
-resource "aws_route53_record" "core_record" {
-  count   = "${var.r53_domain == "" ? 0 : var.core_count}"
-  zone_id = "${data.aws_route53_zone.domain.zone_id}"
-  name    = "${var.project}-${var.name}-core${format("%02d", count.index + 1)}.${data.aws_route53_zone.domain.name}"
-  type    = "A"
-  ttl     = "60"
-  records = ["${module.core.instance_private_ip[count.index]}"]
-}
