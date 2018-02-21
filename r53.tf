96f03f6be6415247d9053d0fc4f2a5af36fd42ae
Philip Dubois <hello@philipdubois.be>
2018-02-21 15:45:14 +0100

    Remove multivalue routing policy

--- r53.tf
+++ r53.tf
@@ -13,12 +13,9 @@ resource "aws_route53_record" "core_record" {
 }
 
 resource "aws_route53_record" "cluster_record" {
-  count                            = "${var.r53_domain == "" ? 0 : var.core_count}"
   zone_id                          = "${data.aws_route53_zone.domain.zone_id}"
   name                             = "${var.project}-${var.name}.${data.aws_route53_zone.domain.name}"
   type                             = "A"
   ttl                              = "60"
-  records                          = ["${module.core.instance_private_ip[count.index]}"]
-  multivalue_answer_routing_policy = true
-  set_identifier                   = "${var.project}-${var.name}-core${format("%02d", count.index + 1)}"
+  records                          = ["${module.core.instance_private_ip}"]
 }
